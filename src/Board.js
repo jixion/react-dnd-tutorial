379ff1a8165f3d473e6607c0a97e4bda6811e1e2
Jeff Burroughs <jburroughs@sparkimaginations.com>
2019-07-18 21:31:35 -0500

    drag and drop implemented

--- src/Board.js
+++ src/Board.js
@@ -1,28 +1,27 @@
 import React from "react";
-import Square from "./Square";
 import Knight from "./Knight";
-import { moveKnight } from "./Game";
+import BoardSquare from "./BoardSquare";
 
-function handleSquareClick(toX, toY) {
-  moveKnight(toX, toY);
-}
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const isKnightHere = x === knightX && y === knightY;
-  const black = (x + y) % 2 === 1;
-  const piece = isKnightHere ? <Knight /> : null;
 
   return (
-    <div
-      key={i}
-      style={{ width: "50px", height: "50px" }}
-      onClick={() => handleSquareClick(x, y)}
-    >
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: "50px", height: "50px" }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
+}
+
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) {
+    return <Knight />;
+  }
 }
 
 export default function Board({ knightPosition }) {
@@ -32,16 +31,18 @@ export default function Board({ knightPosition }) {
   }
 
   return (
-    <div
-      style={{
-        padding: "5px",
-        width: "400px",
-        height: "400px",
-        display: "flex",
-        flexWrap: "wrap"
-      }}
-    >
-      {squares}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{
+          padding: "5px",
+          width: "400px",
+          height: "400px",
+          display: "flex",
+          flexWrap: "wrap"
+        }}
+      >
+        {squares}
+      </div>
+    </DndProvider>
   );
 }
